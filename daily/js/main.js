0907d5e496edec561eb0463b7f0d9d93dd060606
Noah Fisher <nfisher@mica.edu>
2018-04-05 16:17:30 -0400

    Done-1
    For the most part done

--- daily/js/main.js
+++ daily/js/main.js
@@ -1,3 +1,26 @@
-$("#menubutton").click(function() {
-  $("ul").toggleClass("ulDrop");
-});
+function menuAnim() {
+  var topB = document.getElementById('topB');
+  var midB = document.getElementById('midB');
+  var botB = document.getElementById('botB');
+  var navs = document.getElementsByClassName("nav-content");
+  var i;
+  var openNav = navs[i];
+
+  if (topB.classList.contains('topBC')) {
+    topB.classList.remove('topBC');
+    midB.classList.remove('midBC');
+    botB.classList.remove('botBC');
+  } else {
+    topB.classList.add('topBC');
+    midB.classList.add('midBC');
+    botB.classList.add('botBC');
+  }
+  if (!event.target.matches('#menubutton')) {
+    for (i = 0; i < navs.length; i++) {
+      if (openNav.classList.contains('show')) {
+        openNav.classList.remove('show');
+      }
+    }
+  }
+  document.getElementById("navDrop").classList.toggle("show");
+};
